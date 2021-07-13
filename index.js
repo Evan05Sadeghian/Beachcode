d1c0553345e64f58cc3077cec3ad9194a1bfc021
Evan05Sadeghian <evan.sadeghian@icloud.com>
2021-07-13 16:21:29 -0700

    first push

---
+++ index.js
@@ -0,0 +1,11 @@
+var date = "Nov 8";
+
+if (date == "Nov 5") {
+	console.log("Happy Birthday!");
+} else if (date == "Nov 6") {
+	console.log("Happy Belated Birthday!");
+} else if (date == "Nov 8") {
+	console.log("you could have a slice");
+} else if (date == "Nov 9") {
+	console.log("you can't have cake!");
+}
